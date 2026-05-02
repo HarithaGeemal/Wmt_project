36cbf344134ff6a01f6ea1ced67fdd4d814b9165
Haritha Geemal <geemalharitha631@gmail.com>
2026-05-02 13:28:33 +0530

    commit

--- adminpanel_app/adminBackend/middlewares/errorHandler.js
+++
@@ -1,8 +0,0 @@
-import logger from '../utils/logger.js';
-
-function errorHandler(err, req, res, next) {
-    logger.error(`${req.method} ${req.url} - ${err.message}`);
-    res.status(500).json({ error: 'Internal server error' });
-}
-
-export default errorHandler;
